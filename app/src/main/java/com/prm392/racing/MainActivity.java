82e6ec81ff9675afe70930370dbde8609168f8be
nguyenkienminh <kienminh04122003@gmail.com>
2025-06-07 07:33:11 +0700

    Add Button Out

--- app/src/main/java/com/prm392/racing/MainActivity.java
+++ app/src/main/java/com/prm392/racing/MainActivity.java
@@ -1,11 +1,14 @@
 package com.prm392.racing;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,7 +16,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnStart,btnGuide;
+    private Button btnStart,btnGuide, btnThoat;
     private MediaPlayer loginTonePlayer;
 
     @Override
@@ -34,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnStart = findViewById(R.id.btnStart);
         btnGuide = findViewById(R.id.btnGuide);
+        btnThoat = findViewById(R.id.btnThoat);
 
         btnStart.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, RacingActivity.class);
@@ -45,6 +49,22 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, TutorialActivity.class);
             startActivity(intent);
             finish();
+        });
+
+        btnThoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Xác nhận thoát")
+                        .setMessage("Bạn có chắc chắn muốn thoát không?")
+                        .setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                finishAffinity(); // Thoát toàn bộ ứng dụng
+                            }
+                        })
+                        .setNegativeButton("Không", null)
+                        .show();
+            }
         });
 
     }
